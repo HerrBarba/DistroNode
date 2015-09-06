1085c2efd49d1785ccb6203fc9ab0092fcc0531a
Jorge <sky_chainer@hotmail.com>
2015-09-06 12:36:31 -0500

    Client/Server initial functionality

--- Distro/src/Main/Client.groovy
+++ Distro/src/Main/Client.groovy
@@ -1,15 +1,56 @@
 package Main
 
-import java.util.Date;
+import java.io.File;
+
+import javax.xml.XMLConstants
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
 
 class Client {
-
+	
+	static File xsd = new File ("." + File.separator + "MainXSD.xsd");
+	
     static void main(def args) {
-        def s = new Socket("localhost", 4444)
-        s.withStreams { input, output ->
-            def now = Utils.formatDate2String(new Date())
-            output << "<Node><NodeId>is682778</NodeId><Timestamp>$now</Timestamp></Node>"
-            println "response"
-        }
+        def client = new Socket("localhost", 4444)
+
+		client.withStreams { InputStream input, OutputStream output ->
+			// Read message
+			def message = getMessageFromReader(input.newReader())
+			
+			// Validate message
+			validateMessage(new StringReader(message))
+			
+			// Parse message
+			def rootNode = new XmlParser().parse(new StringReader(message))
+			String nodeId = getNodeId(rootNode)
+			String timestamp = getTimestamp(rootNode)
+			
+			// Confirm reception
+			println "Received message from: Node-($nodeId), Timestamp-($timestamp)."
+		}
+    }
+    
+    static boolean validateMessage(Reader message) {    
+        
+        // Create schema from XSD file
+        def factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+        def schema = factory.newSchema(xsd)
+        def validator = schema.newValidator()
+        
+        // Use schema to validate message
+        def source = new StreamSource(message)
+        validator.validate(source)
+    }
+    
+    static String getNodeId(Node rootNode) {
+        return rootNode.NodeId.text()
+    }        
+    
+    static String getTimestamp(Node rootNode) {
+        return rootNode.Timestamp.text()
+    }
+    
+    static String getMessageFromReader(Reader reader) {
+        return reader.readLines().iterator().join("\n");
     }
 }
--- Distro/src/Main/Server.groovy
+++ Distro/src/Main/Server.groovy
@@ -6,7 +6,6 @@ import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.SchemaFactory
 
 class Server {
-    static File xsd = new File ("." + File.separator + "MainXSD.xsd");
     static void main(def args) {
 
         def server = new ServerSocket(4444)
@@ -15,46 +14,12 @@ class Server {
                 println "Waiting for connection..."
                 socket.withStreams { InputStream input, OutputStream output ->
                     
-                    def message = getMessageFromReader(input.newReader())
-                    // Validate message
-                    validateMessage(new StringReader(message))
-                    
-                    // Parse message
-                    def rootNode = new XmlParser().parse(new StringReader(message))
-                    String nodeId = getNodeId(rootNode)
-                    String timestamp = getTimestamp(rootNode)
-                    println "Received message from: Node-($nodeId), Timestamp-($timestamp)."
-                    
-                    // Send response
+                    // Send message
                     def now = Utils.formatDate2String(new Date())
                     output << "<Node><NodeId>is682778</NodeId><Timestamp>$now</Timestamp></Node>"
                 }
-                println "processing/thread complete."
+                println "Request complete."
             }
         }
-    }
-    
-    static boolean validateMessage(Reader message) {    
-        
-        // Create schema from XSD file
-        def factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-        def schema = factory.newSchema(xsd)
-        def validator = schema.newValidator()
-        
-        // Use schema to validate message
-        def source = new StreamSource(message)
-        validator.validate(source)
-    }
-    
-    static String getNodeId(Node rootNode) {
-        return rootNode.NodeId.text()
-    }        
-    
-    static String getTimestamp(Node rootNode) {
-        return rootNode.Timestamp.text()
-    }
-    
-    static String getMessageFromReader(Reader reader) {
-        return reader.readLines().iterator().join("\n");
     }
 }
